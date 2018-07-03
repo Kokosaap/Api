2a2608e36c982729ef15e8623cbea8f4b44a6e64
Kokosaap <leanaelle.salmier@epitech>
2018-07-03 18:06:27 +0200

    first commit

---
+++ src/Entity/Hello.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ *
+ * @ApiResource
+ * @ORM\Entity
+ */
+
+class Hello
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     */
+    public $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
+
+?>
